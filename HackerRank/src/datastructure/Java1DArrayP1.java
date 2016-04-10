a4b5dc7e98e08c8fd421d455c18b4f330a6eb821
epremkaro <epremyan@gmail.com>
2016-04-10 19:33:22 +0300

    All Domains Java Data Structure Java 1D Array (Part 1) Points: 253.00 Rank: 3102

---
+++ HackerRank/src/datastructure/Java1DArrayP1.java
@@ -0,0 +1,30 @@
+package datastructure;
+
+import java.util.Scanner;
+
+public class Java1DArrayP1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] myList = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			myList[i] = sc.nextInt();
+		}
+		
+		int count = 0;
+		for (int i = 0; i < myList.length; i++){
+			int sum = 0;
+			for (int j = i; j < myList.length; j++){
+				sum = sum + myList[j];
+				if (sum < 0)
+					count++;
+			}
+		}
+		System.out.println(count);
+
+	}
+
+}
