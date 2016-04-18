45cfe28dc16f107ab853d2527e38612e073c0a84
epremkaro <epremyan@gmail.com>
2016-04-18 22:01:45 +0300

    Java Stack

---
+++ HackerRank/src/datastructure/JavaStack.java
@@ -0,0 +1,46 @@
+package datastructure;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class JavaStack {
+	
+	private Stack st = new Stack();
+	public final String[] openPar = {"{", "(", "["};
+	public final String[] closePar = {"}", ")", "]"};
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		JavaStack js = new JavaStack();  
+		while (sc.hasNext()) {
+			js.st.clear();
+			String input = sc.next();
+			System.out.println(js.isBalanced(input));
+		}
+	}
+
+	private boolean isBalanced(String input) {
+		
+		String[] strs = input.split("");
+		for (String s:strs){
+			for (String op:openPar)
+				if (s.equals(op))
+					st.push(op);
+			
+			for (String op:closePar){
+				if (s.equals(op)){
+					if (st.empty())
+						return false;
+					else
+						st.pop();
+				}
+			}
+		}
+		
+		if (st.empty())
+			return true;
+			
+		return false;
+	}
+
+}
