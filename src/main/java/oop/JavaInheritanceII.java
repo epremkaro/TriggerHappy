5a63564ef624dbb7e6546432a9770256ac7eeef3
kyepremyan <v-kyepremyan@expediagroup.com>
2018-12-01 18:57:23 +0300

    ECPR-59300: switched to ers-maven repositories

--- src/main/java/oop/JavaInheritanceII.java
+++ src/main/java/oop/JavaInheritanceII.java
@@ -20,7 +20,7 @@ class Solution {
 
 	public static void main(String[] argh) {
 		Adder X = new Adder();
-		System.out.println("My superclass is: "
+		System.out.println("The superclass is - : "
 				+ X.getClass().getSuperclass().getName());
 		System.out.print(X.add(10, 32) + " " + X.add(10, 3) + " "
 				+ X.add(10, 10) + "\n");
